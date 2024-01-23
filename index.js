a7fa1602e69a3e3d23f420fcd7a977c91278679f
HawkEye201 <imt_2019024@iiitm.ac.in>
2024-01-23 15:38:06 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Connected");
+});
+
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
